9ae49d98afb8e91f801b6a2cee2caefc6e7cc6a8
HuntleyWeb <huntleyweb@hotmail.com>
2021-06-30 20:41:52 +0100

    Settings

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  mailApiEndpoint: 'https://hwebapi.azurewebsites.net/api/Email/sendmessage',
+  mailServerUser: 'jon.huntley@huntleyweb.co.uk',
+  contactUsTarget: 'jon.huntley@hotmail.co.uk',
+  notificationList: ''
 };
